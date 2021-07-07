6453c4edf2939bfe86a852e61d053eeb21e1eef4
jonyszone <jonyszone@gmail.com>
2021-07-07 13:34:01 +0600

    updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text ('my first app'),
-        centerTitle: true,
-        backgroundColor: Colors.green.shade400,
-      ),
-      body: Center (
-        child: Text("It's me Shafi"),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
+  runApp(MyApp());
+}
 
-        },
-        child: Text('click'),
-        backgroundColor: Colors.black87,
+class MyApp extends StatelessWidget {
+  build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('my first app'),
+          centerTitle: true,
+          backgroundColor: Colors.green.shade400,
+        ),
+        body: Center(
+          child: Text("It's me Shafi"),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {},
+          child: Text('click'),
+          backgroundColor: Colors.black87,
+        ),
       ),
-    ),
-  ));
+      debugShowCheckedModeBanner: false,
+    );
+  }
 }
