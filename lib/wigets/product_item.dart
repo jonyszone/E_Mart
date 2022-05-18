c59886581120686d3f99b4c2a8747977ac9c67a1
Shafi Ul Islam <jonyszone@gmail.com>
2022-05-18 12:13:38 +0600

    updated the project to recent updates of versions

--- lib/wigets/product_item.dart
+++ lib/wigets/product_item.dart
@@ -10,7 +10,10 @@ class ProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GridTile(
-      child: Image.network(imageUrl),
+      child: Image.network(
+        imageUrl,
+        fit: BoxFit.cover,
+      ),
     );
   }
 }
