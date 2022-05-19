92d3c4179f17dc69a5945751652d4c67dcb0d779
Shafi Ul Islam <jonyszone@gmail.com>
2022-05-19 10:42:30 +0600

    added fab and cart button on product

--- lib/wigets/product_item.dart
+++ lib/wigets/product_item.dart
@@ -14,6 +14,23 @@ class ProductItem extends StatelessWidget {
         imageUrl,
         fit: BoxFit.cover,
       ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(
+            Icons.shopping_cart,
+          ),
+          onPressed: () {},
+        ),
+      ),
     );
   }
 }
